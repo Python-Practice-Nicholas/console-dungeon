9f8ba44f859a696e3100cb934317e7464e419077
NicholasKoblitz <68592588+NicholasKoblitz@users.noreply.github.com>
2024-02-20 14:12:42 -0500

    created monster class

---
+++ src/classes/monster.py
@@ -0,0 +1,21 @@
+from Entity import Entity
+import json
+
+
+class Monster(Entity):
+	
+	@staticmethod
+	def create_monster(name):
+		with open('../json/monsters.json') as file:
+			data = json.load(file)
+			monster_data = data[name]
+			health = monster_data["health"]
+			strength = monster_data["strength"]
+			speed = monster_data["speed"]
+			agility = monster_data["agility"]
+			weapon = monster_data["weapon"]
+			inventory = monster_data["inventory"]
+			
+			monster = Monster(name, health, strength, speed, agility, weapon, inventory)
+			return monster
+			
