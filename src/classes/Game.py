e72011d12368e53b007c7ff5bfa76bcfda5c8836
Nicholas <nicholas@Sabrinas-MacBook-Pro.local>
2024-02-21 18:53:13 -0500

    Started to layout structure of Game class

---
+++ src/classes/Game.py
@@ -0,0 +1,20 @@
+from Player import Player
+from monster import Monster
+from Item import Item
+import json
+
+class Game():
+    
+    player = {}
+
+    def start(self):
+        pass
+
+    def end(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
