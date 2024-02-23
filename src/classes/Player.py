d9018ec816fa45275b11e0174ceff33b3b06a5d1
Nicholas <nicholas@Sabrinas-MacBook-Pro.local>
2024-02-22 21:48:50 -0500

    Added doc strings to methods and put in two methods that will need to be fleshed out"

--- src/classes/Player.py
+++ src/classes/Player.py
@@ -4,9 +4,11 @@ from Item import Item
 class Player(Entity):
 		
 	def get_weapon(self):
+		"""Returns the Player's Weapon"""
 		return self._weapon
 		
 	def set_weapon(self, weapon_name):
+		"""Sets the player's current weapon"""
 		weapon = self.get_single_item(weapon_name)
 		if weapon['type'] != "weapon":
 			return f"{weapon.name} is not a weapon"
@@ -16,6 +18,7 @@ class Player(Entity):
 		
 	
 	def set_inventory_item(self, item):
+		"""Adds an item to the Player's inventory"""
 		self._inventory.update(
 			{
 				item.get_name(): 
@@ -29,4 +32,13 @@ class Player(Entity):
 		return self._inventory
 		
 	def remove_inventory_item(self,item_name):
+		"""Removes an item from the Player's inventory"""
 		self._inventory.pop(item_name)
+
+	def display_stats(self):
+		"""Displays the Player's current stats"""
+		pass
+
+	def player_save_data(self):
+		"""Returns the Player's information in a savable format"""
+		pass
